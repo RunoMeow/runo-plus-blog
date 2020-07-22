75ea38966bf0266553d7757d089eabcdfc5ae430
RunoMeow <550000714@qq.com>
2020-07-22 23:58:26 +0800

    添加启动脚本

---
+++ index.js
@@ -0,0 +1,9 @@
+const { exec } = require('child_process');
+exec('hexo server -p 4000 & ', (error, stdout, stderr) => {
+  if (error) {
+    console.log('exec error: ${error}');
+    return;
+  }
+  console.log('stdout: ${stdout}');
+  console.log('stderr: ${stderr}');
+});
