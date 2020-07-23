c84e8a0f14aa0412802dccea4944a936adeb2815
RunoMeow <550000714@qq.com>
2020-07-23 12:36:42 +0800

    发布测试文章~

---
+++ source/_posts/test.md
@@ -0,0 +1,9 @@
+---
+title: test
+date: 2020-07-23 12:34:47
+tags:
+  - other
+categories:
+  - other
+---
+测试测试w
--- webhook.js
+++ webhook.js
@@ -1,13 +1,19 @@
 const express = require('express');
 const app = express();
 const nodeCmd = require('node-cmd');
-const crypto = require('crypto')
+const crypto = require('crypto');
 require('dotenv').config();
 
-app.use(express.json())
+app.use(express.json());
 app.post('/apis/update', (req, res) => {
-  const sign = req.headers['x-hub-signature']
-  const sha1 = 'sha1=' + crypto.createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET).update(JSON.stringify(req.body)).digest().toString('hex')
+  const sign = req.headers['x-hub-signature'];
+  const sha1 =
+    'sha1=' +
+    crypto
+      .createHmac('sha1', process.env.GITHUB_WEBHOOK_SECRET)
+      .update(JSON.stringify(req.body))
+      .digest()
+      .toString('hex');
   if (!sign || sign !== sha1) return res.send('secret error');
   nodeCmd.get('git pull', (err, data) => {
     if (!err) {
