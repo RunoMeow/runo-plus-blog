cb27be23c580eeb5e8d0d8d07eb01b29dd6c0531
RunoMeow <550000714@qq.com>
2020-07-23 11:14:22 +0800

    webhookBUG

--- webhook.js
+++ webhook.js
@@ -3,7 +3,21 @@ const app = express();
 const nodeCmd = require('node-cmd');
 require('dotenv').config();
 
-app.get('/', (req, res) => {
+app.get('/apis/update', (req, res) => {
+  console.log('get', req.query);
+  nodeCmd.get('git pull', (err, data, stderr) => {
+    if (!err) {
+      console.log(data);
+      nodeCmd.get('npx hexo generate', (err, data, stderr) => {
+        if (err) console.error(err);
+        else console.log(data);
+      });
+    } else console.error(err);
+  });
+  res.send('success');
+});
+
+app.get('/apis/update', (req, res) => {
   console.log('post', req.body);
   nodeCmd.get('git pull', (err, data, stderr) => {
     if (!err) {
